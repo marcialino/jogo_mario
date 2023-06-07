806ae9833eb55773d13e069c4b85ade8409179be
marcialino <marcialino100@gmail.com>
2023-06-07 11:35:00 -0300

    atualizei

--- js/script.js
+++ js/script.js
@@ -12,13 +12,15 @@ const jump = ()=>{
 
 const loop = setInterval(()=>{
 
-    const pipePosition = pipe.offsetLeft
+    const pipePosition = $('.pipe').offset().left
+
 
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('p', '')
 
-    if(pipePosition <=120 && pipePosition > 0 && marioPosition < 80){
+    if(pipePosition <=120 && pipePosition > 0 && marioPosition < 80
+    ){
         pipe.style.animation = 'none'
-        pipe.style.left =`${pipePosition}px `
+        pipe.style.left =`${pipePosition}px`
 
         mario.style.animation = 'none'
         mario.style.bottom =`${marioPosition}px `
