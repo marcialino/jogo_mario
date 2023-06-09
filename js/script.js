ca2f471f26de6199fd13becca170bdfa9ac48dc5
marcialino <marcialino100@gmail.com>
2023-06-09 15:23:54 -0300

    atualiei jogo_mario

--- js/script.js
+++ js/script.js
@@ -8,6 +8,21 @@ audioStart = new Audio('./imagens/audio_theme.mp3')
 
 audioGameOver = new Audio('./imagens/audio_gameover.mp3')
 
+const start =()=>{
+
+    document.getElementById('text-start').style.color ='rgb(236, 236, 236)'
+    
+    pipe.classList.add('pipe-animation')
+
+    /*mario.src ='./imagens/mario.gif'
+    mario.style.width= '150px'
+    mario.style.marginLeft ='50px'*/
+
+    audioStart.play()
+
+}
+document.addEventListener('keydown',start)
+
 const jump = ()=>{
     mario.classList.add('jump')
 
@@ -15,8 +30,45 @@ const jump = ()=>{
         mario.classList.remove('jump')
     }, 1500)
 }
+document.addEventListener('keydown', jump)
 
-const loop = setInterval(()=>{
+
+/*acabar com o jogo*/
+
+const checkGameOver = setInterval(()=>{
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+
+    if(pipePosition <=120 && pipePosition > 0 && marioPosition < 80){
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = './imagens/mario1.jpg'
+        mario.style.width= '75px'
+        mario.style.marginLeft ='50px'
+
+        document.getElementById('text-start').style.color='black'
+        document.getElementById('text-start').innerHTML='<strong>GAME OVER</strong>'
+
+        function stopAudioStart(){
+            audioStart.pause()
+        }stopAudioStart()
+
+        audioGameOver.play()
+
+        function stopAudio(){
+            audioGameOver.pause()
+        }setTimeout(stopAudio,8000)
+
+        clearInterval(checkGameOver)
+    }
+
+},10)
+
+/*const loop = setInterval(()=>{
 
     const pipePosition = pipe.offsetLeft
    
@@ -30,13 +82,14 @@ const loop = setInterval(()=>{
         mario.style.animation = 'none'
         mario.style.bottom =`${marioPosition}px `
 
-
         mario.src = './imagens/mario1.jpg'
         mario.style.width = '75px'
         mario.style.marginLeft ='50px'
 
-        document.getElementById('text-start').style.color = 'black'
-        document.getElementById('text-start').innerHTML ='<strong>GAME OVER</strong>'
+        document.getElementById('text-start').style.color= 'black'
+        document.getElementById('text-start').innerHTML='<strong>GAME OVER</strong>'
+
+
 
         function stopAudioStart(){
             audioStart.pause()
@@ -54,4 +107,4 @@ const loop = setInterval(()=>{
 
 }, 10)
 
-document.addEventListener('keydown', jump)
+document.addEventListener('keydown', jump)*/
